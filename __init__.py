de615908809d4ae546d4aabe1a494decfcf8262e
jmartin <jmartin@zikzakmedia.com>
2014-03-07 14:38:55 +0100

    __init__.py pep8 adapted

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF-8 -*-
-#This file is part electronic_mail module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part of electronic_mail module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
 from electronic_mail import *
+
 
 def register():
     Pool.register(
