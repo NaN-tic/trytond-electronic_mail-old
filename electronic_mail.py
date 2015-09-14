fe4c3a44288f4770b573062847aaa78ab44eb984
aneolf <aneolf@yahoo.es>
2015-09-14 12:46:17 +0200

    Revert last commit

--- electronic_mail.py
+++ electronic_mail.py
@@ -642,12 +642,14 @@ class ElectronicMail(ModelSQL, ModelView):
         return dict([(mail.id, mail.mailbox.user.id) for mail in mails])
 
     @classmethod
-    def get_mailbox_users(cls, records, names):
+    def get_mailbox_users(cls, records, name):
+        assert name in ('mailbox_read_users', 'mailbox_write_users')
         res = {}
-        for name in names:
-            assert name in ('mailbox_read_users', 'mailbox_write_users')
-            for mail in records:
-                res[name][mail.id] = [x.id for x in mail.mailbox[name[8:]]]
+        for mail in records:
+            if name == 'mailbox_read_users':
+                res[mail.id] = [x.id for x in mail.mailbox['read_users']]
+            else:
+                res[mail.id] = [x.id for x in mail.mailbox['write_users']]
         return res
 
     @classmethod
