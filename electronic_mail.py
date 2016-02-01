eb54f9b6261f5bd68cf12a34021ca6ea029b6e7a
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-02-01 14:06:10 +0100

    Correctly cast binary data
    (grafted from 69f01cc9231f0dfedd243066cfb84488b6e28981)

--- electronic_mail.py
+++ electronic_mail.py
@@ -683,12 +683,15 @@ class ElectronicMail(ModelSQL, ModelView):
             result[fname] = {}
         for mail in mails:
             email_file = mail._get_email() or None
-            result['email_file'][mail.id] = email_file
+            result['mail_file'][mail.id] = fields.Binary.cast(mail_file)
             email = msg_from_string(email_file)
             body = cls.get_body(mail, email)
             result['body_plain'][mail.id] = body.get('body_plain')
             result['body_html'][mail.id] = body.get('body_html')
             result['num_attach'][mail.id] = len(cls.get_attachments(email))
+        for fname in ['body_plain', 'body_html', 'num_attach', 'mail_file']:
+            if fname not in names:
+                del result[fname]
         return result
 
     @classmethod
