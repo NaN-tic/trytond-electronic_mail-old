a5413c245be856b40be7d58e08e3cf57ab7c54d7
resteve <resteve@zikzakmedia.com>
2016-02-01 14:12:49 +0100

    FIX typo 5b3e13d8956b (2)

--- electronic_mail.py
+++ electronic_mail.py
@@ -683,13 +683,13 @@ class ElectronicMail(ModelSQL, ModelView):
             result[fname] = {}
         for mail in mails:
             email_file = mail._get_email() or None
-            result['mail_file'][mail.id] = fields.Binary.cast(email_file)
+            result['email_file'][mail.id] = fields.Binary.cast(email_file)
             email = msg_from_string(email_file)
             body = cls.get_body(mail, email)
             result['body_plain'][mail.id] = body.get('body_plain')
             result['body_html'][mail.id] = body.get('body_html')
             result['num_attach'][mail.id] = len(cls.get_attachments(email))
-        for fname in ['body_plain', 'body_html', 'num_attach', 'mail_file']:
+        for fname in ['body_plain', 'body_html', 'num_attach', 'email_file']:
             if fname not in names:
                 del result[fname]
         return result
